c566eb8b467a5050f8d4cf4f344a0674f79e590f
hidayatunnisa071 <hdayatunnisa071@gmail.com>
2024-09-20 18:21:51 +0800

    Add files via upload

---
+++ Variabel002.java
@@ -0,0 +1,32 @@
+public class Variabel002 {
+    public static void main(String[] args) {
+        // int bil1;
+        // int bil2;
+        // int hassil;
+        double bil1, bil2, hasil = 0; // gabungan baris 3-5
+        String nama = "";
+
+        // input nilai
+        bil1 = 7;
+        bil2 = 11;
+
+        // proses data
+        // hasil = bil1 + bil2; // variabel_penampung op_assigment var op var
+        hasil = bil1 - bil2;
+        // hasil = bil1 * bil2;
+        // hasil = bil1 / bil2;
+        
+        // modulus %, sisa hasil bagi
+        // hasil 5 % 2
+        hasil = 5 % 2;
+
+        // output
+        System.out.println("bilangan 1: " + bil1);
+        System.out.println("bilangan 2: " + bil2);
+        System.out.println(hasil);
+
+       
+
+    }
+    
+}
